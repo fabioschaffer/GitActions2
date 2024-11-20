f42985f9421294ca4fb16dd9258dce741d834a04
Fabio Schaffer <fabioschaffer@gmail.com>
2024-11-20 11:31:54 -0300

    2

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 app.MapGet("/product", () => {
-    return new string[] { "Mouse", "Teclado" };
+    return new string[] { "Mouse 2" };
 
 });
 app.Run();
